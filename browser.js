a31a5813e0717d74e277299ce540adc1ba61a149
Leon Zagorouiko <Zagorouiko@gmail.com>
2023-09-09 08:54:27 -0700

    updated browser

--- browser.js
+++ browser.js
@@ -5,7 +5,7 @@ async function startBrowser(){
 	try {
 	    console.log("Opening the browser......");
 	    browser = await puppeteer.launch({
-	        headless: 'new',
+	        headless: true,
 	        args: ["--disable-setuid-sandbox"],
 	        ignoreHTTPSErrors: true
 	    });
