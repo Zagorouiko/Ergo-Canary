27283b613f9b64160cd0ed45b5d5adcdaa193183
Leon Zagorouiko <Zagorouiko@gmail.com>
2023-09-09 09:07:36 -0700

    updated browser

--- index.js
+++ index.js
@@ -74,11 +74,7 @@ async function startBrowser(){
 	let browser;
 	try {
 	    console.log("Opening the browser......");
-	    browser = await puppeteer.launch({
-	        headless: true,
-	        args: ["--disable-setuid-sandbox"],
-	        ignoreHTTPSErrors: true
-	    });
+	    browser = await puppeteer.launch();
 	} catch (err) {
 	    console.log("Could not create a browser instance => : ", err);
 	}
